156df1d016578d76e6c478d86e356c24728ed8ae
Abhishek kumar <151649357+meaviishek@users.noreply.github.com>
2024-09-27 09:10:34 +0530

    Update Header.jsx

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full bg-[#8c8c8c51] shadow-md z-50 backdrop-blur-xl">
-      <div className="container mx-auto flex justify-between items-center py-4 px-6">
+      <div className="container mx-auto flex justify-between items-center py-4 px-2">
         {/* Logo */}
         <Link to='/'>
           <div className="flex items-center">
@@ -43,7 +43,7 @@ const Header = () => {
         {/* Mobile Menu Button */}
         <div className="md:hidden">
           <button onClick={toggleMenu} className="text-primary focus:outline-none">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
