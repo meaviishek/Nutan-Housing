6b8b8bdbbe9e4e82cc43bc38b6196f8bdb182ee0
Meeaviishek <155694061+Meeaviishek@users.noreply.github.com>
2024-09-27 22:42:33 +0530

    first commit

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = ({openLogin,openEnq}) => {
   
   return (
     <header className="fixed top-0 left-0  w-full bg-[#8c8c8c51] shadow-md z-50 backdrop-blur-xl">
-      <div className="container mx-auto flex justify-between items-center py-4 ">
+      <div className="container mx-auto flex justify-between items-center py-4 px-4">
         {/* Logo */}
         <Link to='/'>
           <div className="flex items-center">
