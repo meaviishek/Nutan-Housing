adbb9fa86fde1bee7c37393503fd534da2ceccf3
Meeaviishek <155694061+Meeaviishek@users.noreply.github.com>
2024-09-29 02:44:01 +0530

    first commit

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -2,6 +2,8 @@
 import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { Logo } from '.';
+import { MdEmail, MdLocationOn, MdPhone } from "react-icons/md";
+
 function Footer({openLogin,openEnq}) {
   return (
     <div>
@@ -10,7 +12,7 @@ function Footer({openLogin,openEnq}) {
 
 
 
-    <footer className="bg-[#181818] text-white py-10">
+    <footer className="bg-[#181818] text-white pt-8">
     <div className="container max-w-7xl md:mx-auto px-4  grid grid-cols-1 lg:grid-cols-3 gap-8  ">
       
       {/* Company Info */}
@@ -24,9 +26,14 @@ function Footer({openLogin,openEnq}) {
           <h2 className="text-md ml-2 font-bold">Nutan Housing Finance</h2>
         </div>
         <div className="text-gray-400 text-sm">
-          <p className='mt-2'><a href="mailto:info@nutanhousing.com" className="hover:text-white">info@nutanhousing.com</a></p>
-          <p className='mt-2'><a href="tel:+919471613137" className="hover:text-white ">+91 9471613137</a> <a href="tel:+918539804930" className="hover:text-white">+91 8539804930</a></p>
-          <p className='mt-2'>Jhunki Baba Road, Patel Nagar,<br />Near RDH School, Rajgir, 803116</p>
+        <p className='mt-2 gap-2 flex'>
+         <MdLocationOn className=''/>  Jhunki Baba Road, Patel Nagar,<br />Near RDH School, Rajgir, 803116</p>
+          <p className='mt-2 gap-2 flex'>
+           <MdEmail/> <a href="mailto:info@nutanhousing.com" className="hover:text-white">info@nutanhousing.com</a></p>
+          <p className='mt-2 gap-2 flex'>
+          
+           <MdPhone/> <a href="tel:+919471613137" className="hover:text-white ">+91 9471613137</a> <a href="tel:+918539804930" className="hover:text-white">+91 8539804930</a></p>
+          <p className='mt-2 text-primary'>Open On Sunday</p>
         </div>
       </div>
 
@@ -71,7 +78,7 @@ function Footer({openLogin,openEnq}) {
     </div>
 
     {/* Footer Bottom */}
-    <div className="mt-6 border-t border-gray-800 pt-4 text-center text-gray-500 text-sm">
+    <div className="mt-4 border-t border-gray-800 pt-4 text-center text-gray-500 text-sm">
       <p>© Nutan Housing Finance 2024, All rights reserved.</p>
       <p>Developed by <a href="https://prachar.com" target="_blank" rel="noreferrer" className="hover:text-primary">prachar.com</a></p>
     </div>
